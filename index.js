9c29ecb740f8be5d1d2a81e7d08bc2ce383f6527
François CARIOU <fanch-github@qth.fr>
2016-11-19 13:45:21 +0100

    Add sample help page

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res){
   '<p>Welcome !</p>');
 });
 
+app.get('/help', function(req, res){
+  res.sendFile(__dirname + '/templates/help.html');
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
